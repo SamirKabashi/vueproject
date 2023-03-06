c98bfa0caf9f017cd03a9691f389b5b498796bb7
SamirKabashi <93317462+SamirKabashi@users.noreply.github.com>
2023-03-06 14:00:15 +0100

    views

---
+++ client/src/views/AboutView.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-container>
+    <p> <strong>NARUTO</strong> <br />
+      Naruto Uzumaki is a young boy from the Hidden Leaf Village and the biggest problem child at the ninja academy.
+      He causes mischief every day and is constantly being scolded by his teacher, Iruka Umino. He has fallen so far
+      behind in his studies that his graduation is uncertain, but Naruto actually dreams of becoming the leader of the
+      village, known as the Hokage, and vows to one day become the No. 1 ninja.
+      However, all the adults in the village treat Naruto with disdain due to a secret surrounding his birth. 12 years
+      ago, when Naruto was just a baby, the Nine-Tailed Fox attacked the village, and the only way to stop it was to
+      seal it inside of him. This fact is first revealed to him by Mizuki, an instructor at the academy who uses
+      Naruto to steal the Scroll of Sealing from the current Hokage. Once the deed is done, he appears before the
+      young ninja and discloses the shocking truth behind his birth. Naruto manages to defeat Mizuki with Iruka’s
+      help, who despite knowing about Naruto’s past, has always believed in him. After learning Iruka’s true feelings,
+      Naruto gains a renewed determination to pursue his dream.
+      After successfully graduating from the academy alongside fellow students Sasuke Uchiha and Sakura Haruno, Naruto
+      finally takes the first steps on his journey as a real ninja!
+    </p>
+    <p> <strong>BORUTO</strong> <br />
+      <strong>-NARUTO NEXT GENERATIONS-</strong>
+      <br />
+      The Great Ninja War that shook the entire world has now faded into memory along with the many shinobi whose lives it
+      cost, and the Hidden Leaf Village has entered an age of peace and prosperity with Naruto Uzumaki as the Seventh
+      Hokage. Meanwhile, his son, Boruto Uzumaki, struggles with living in the shadow of his remarkable father.
+      Though Boruto may rebel against Naruto’s wishes, he also longs to be recognized by his father as a shinobi and forms
+      his own three-man squad with Sarada and Mitsuki to take the Chunin Exams.
+      To that end, he undergoes training from Sasuke, Sarada’s father and Naruto’s former rival, to learn the ways of
+      ninjutsu.
+      However, the exams prove too difficult for Boruto, and he resorts to using a scientific ninja tool, despite such
+      items being prohibited…
+      Meanwhile, unbeknownst to Boruto and his friends, a new threat looms just over the horizon…!
+      It’s time for the children of legendary ninja past to walk their own ninja way!!
+    </p>
+  </v-container>
+</template>
---
+++ client/src/views/Watch.vue
@@ -0,0 +1,48 @@
+<template>
+    <v-row justify="center">
+      <div>
+        <div class="subheading">
+            <br>
+            <p></p>
+          <p>This is time we are live </p>
+        </div>
+        <v-date-picker
+          v-model="date1"
+          :events="arrayEvents"
+          event-color="green lighten-1"
+        ></v-date-picker>
+      </div>
+    </v-row>
+  </template>
+  
+  <script>
+    export default {
+      data: () => ({
+        arrayEvents: null,
+        date1: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
+        date2: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
+      }),
+  
+      mounted () {
+        this.arrayEvents = [...Array(6)].map(() => {
+          const day = Math.floor(Math.random() * 30)
+          const d = new Date()
+          d.setDate(day)
+          return d.toISOString().substr(0, 10)
+        })
+      },
+  
+      methods: {
+        functionEvents (date) {
+          const [,, day] = date.split('-')
+          if ([12, 17, 28].includes(parseInt(day, 10))) return true
+          if ([1, 19, 22].includes(parseInt(day, 10))) return ['red', '#00f']
+          return false
+        },
+      },
+    }
+  </script>
+
+  <style>
+
+</style>
